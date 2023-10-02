b46016eaa71929ebde9a0d8c6ec5515aed34713a
Gabs <104294505+Hashiexe34@users.noreply.github.com>
2023-10-02 17:13:00 -0300

    Add files via upload

---
+++ Olá, Mundo 2!.py
@@ -0,0 +1,9 @@
+# Solicita Números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo Número: "))
+
+# Realiza operações
+soma = num1 + num2
+
+# Exibe os resultados
+print(f"Soma: {soma}")
